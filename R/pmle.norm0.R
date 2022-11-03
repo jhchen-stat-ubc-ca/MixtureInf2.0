465092ed3db09798fbb5d9df852c661f101a2c57
tom tang <ttang11161999@gmail.com>
2022-11-03 13:22:31 -0700

    nov 3, 2022 v4

--- R/pmle.norm0.R
+++ R/pmle.norm0.R
@@ -4,16 +4,16 @@
 #' variance. When the level of penalty is 0, PMLE reduces to MLE.
 #' @param x The data that can either be a vector or a matrix with the 1st column being the observed values
 #' and the 2nd column being the corresponding frequencies.
-#' @param var A known component variance.
-#' @param m0 The order of the finite mixture model, default value: m0 = 1.
-#' @param lambda The size of the penalized function of the mixing distribution, default value: lambda = 0.
-#' @param inival The initial values chosen for the EM-algorithm, a 3m0-dimension vector including m0 mixing proportions, 
+#' @param var.sub A known subpopulation variance, default 1. 
+#' @param m0 The order of the finite mixture model.
+#' @param lambda The size of the penalty function of the mixing proportions.
+#' @param ini.val The initial values chosen for the EM-algorithm, a 3m0-dimension vector including m0 mixing proportions, 
 #' m0 component means and m0 component variances, or a matrix with 3m0 columns, default value: inival = NULL. (if not provided, random initial values are used.)
-#' @param len The number of initial values chosen for the EM-algorithm, default value: len = 10.
-#' @param niter The smallest number of iterations required for all initial values in the EM-algorithm. 
-#' The algorithm runs EM-iteration niter times from each initial value. 
-#' The iteration will restart from the parameter value with the highest likelihood value at the point and run until convergence. default value: niter = 50.
-#' @param tol The tolerance value for the convergence of the EM-algorithm, default value: tol = 1e-6.
+#' @param n.init The	number of initial values for the EM-algorithm.
+#' @param n.iter The number of EM iterations for all initial values.
+#' @param max.iter  Maximum amount of EM iterations, stops at 5000.
+#' @param tol The tolerance value for the convergence of the EM-algorithm, default value: tol = 1e-8.
+#' @param iter.n This reports the number of EM-iteration employed.
 #' @param rformat The format of the output. If rformat=T, 
 #' it means the output format is determined by R software. 
 #' If rformat=F, it means the output format is determined by our default setting. 
@@ -28,31 +28,43 @@
 #' @export
 #'
 #' @examples 
-pmle.norm0 <-
-  function(x,var,m0=1,lambda=0,inival=NULL,len=10,niter=50,tol=1e-6,rformat=FALSE)
+pmle.norm0 <- 
+  function(x, var.sub = 1, m0, lambda=1, init.val = NULL, 
+           n.init = 10, n.iter=50, max.iter = 5000,
+           tol=1e-8, rformat = FALSE)
   {
     if(m0==1) stop("You do not need this function for MLE")
     
     if (is.vector(x)) xx = x     ## plain vector
     if (is.matrix(x)) { xx=c()
-    for (i in 1:nrow(x)) xx=c(xx, rep(x[i,1],x[i,2]))  }       
+    for (i in 1:nrow(x)) xx=c(xx, rep(x[i,1],x[i,2]))  } 
+    xx = xx/sqrt(var.sub);   ###standardize with known var 
     ## translate the frequency into a plain vector.
-    xx=x/sqrt(var);   ###standardize with known var 
+    nn = length(xx)
     min.x = min(xx); max.x = max(xx)
     
     if (is.null(init.val)) {
-      init.val = matrix(0, nrow = n.init, ncol = 2*m0)
+      init.val = list()
       for(i in 1:n.init) {
         alpha = runif(m0)
-        init.val[i,] = c(alpha/sum(alpha), runif(m0, min.x, max.x))
+        alpha = alpha/sum(alpha)
+        theta = runif(m0, min.x, max.x)
+        init.val[[i]] = rbind(alpha, theta)
       }	
+    } else { n.init = 1; 
+    temp = init.val
+    init.val = list()
+    init.val[[1]]= temp
+    init.val[[1]][2,] = init.val[[1]][2,]/sqrt(var.sub)
     }
     
-    out = pmle.norm0.sub(xx, m0, lambda, init.val, n.init, n.iter, max.iter, tol)
+    out = pmle.norm0.sub(xx, m0, lambda=1, 
+                         init.val, n.init, n.iter, max.iter, tol)
+    
     alpha = out$alpha
-    theta = out$theta*sqrt(var)
-    loglik = out$loglik-n/2*log(var)
-    ploglik = out$ploglik-n/2*log(var)
+    theta = out$theta*sqrt(var.sub)
+    loglik = out$loglik-nn/2*log(var.sub)
+    ploglik = out$ploglik-nn/2*log(var.sub)
     iter.n = out$iter.n
     
     if (rformat==F)
@@ -63,14 +75,16 @@ pmle.norm0 <-
       ploglik=rousignif(ploglik)
     }
     
-    if (lambda==0 | m0==1)
+    if (lambda==0)
       list('MLE of mixing proportions:'= alpha,
-           'MLE of component parameters:'= theta,
+           'MLE of subpop means:'= theta,
            'log-likelihood:'=loglik,
-           'iter.n'= iter.n)
+           'Penalized log-likelihood:'= loglik,
+           'iter.n'= iter.n,
+      )
     else
       list('PMLE of mixing proportions:'= alpha,
-           'PMLE of component parameters:'= theta,
+           'PMLE of subpop means:'= theta,
            'log-likelihood:'= loglik,
            'Penalized log-likelihood:'= ploglik,
            'iter.n'= iter.n)
